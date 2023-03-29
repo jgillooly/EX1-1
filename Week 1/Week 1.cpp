92d7f9f010b1b5f299a289de68eef0a92c4fcb40
Joshua <jgillooly@student.neumont.edu>
2023-03-29 13:40:34 -0600

    who knows

--- Week 1/Week 1.cpp
+++ Week 1/Week 1.cpp
@@ -1,4 +1,4 @@
-// EX1-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Week 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -6,7 +6,6 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "Git\n"
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
